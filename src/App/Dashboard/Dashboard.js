82052c0cefec2c2e441e7e62600600205828343f
Jacek Panasiuk <panasiuk.jc@gmail.com>
2020-01-11 21:19:19 +0100

    Add dummy history overview to dashboard Allow to continue test in progress from dashboard

--- src/App/Dashboard/Dashboard.js
+++ src/App/Dashboard/Dashboard.js
@@ -1,9 +1,38 @@
+/** @jsx jsx */
 import React from 'react'
+import { Link } from 'react-router-dom'
+import { css, jsx } from '@emotion/core'
+import { buttonStyle } from '../Button/Button'
+import { useLocalStorage } from '../../Storage/useLocalStorage'
 
 export function Dashboard() {
+  const [testHistory] = useLocalStorage('fuxi-testHistory', [])
+  const [test] = useLocalStorage('fuxi-test', null)
+
   return (
     <div>
       <h1>Dashboard</h1>
+      {test && (
+        <Link to="/test" css={cssContinueButton}>
+          Continue
+        </Link>
+      )}
+      {testHistory && testHistory.length > 0 && (
+        <React.Fragment>
+          <p>Your pathetic results so far:</p>
+          <ul>
+            {testHistory.map(th => (
+              <li key={th.date}>{th.answers.length} questions answered</li>
+            ))}
+          </ul>
+        </React.Fragment>
+      )}
     </div>
   )
 }
+
+const cssContinueButton = css`
+  ${buttonStyle};
+  background: indigo;
+  color: white;
+`
